220704390965f3b24a33b9f9908c72dbdd0e5df4
Shunshun94 <shunshun9460@gmail.com>
2024-03-26 01:18:49 +0900

    add param

--- python/myconfiguration.default.py
+++ python/myconfiguration.default.py
@@ -1,8 +1,11 @@
 ###########################
 # このファイルを myconfiguration.py にリネームし、
-# PUBLIC_KEY に Discord Application > General Information > PUBLIC KEY の値を代入して使用します
+# PUBLIC_KEY に Discord Application > General Information > PUBLIC KEY の値
+# ACCESS_TOKEN に Discord Application > Bot > TOKEN の値を代入して使用します
 # 
 
 PUBLIC_KEY = ""
+ACCESS_TOKEN = ""
 BROWSER_ACCESS_HEADER = "Discord app によるアクセスを受け付けるようにここは作られています"
 BROWSER_ACCESS_TEXT = "なのでブラウザでアクセスしても何も見せられません＞＜"
+
