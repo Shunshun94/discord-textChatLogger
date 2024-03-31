863e654e24559db0776200fff333a607fcf33227
Shunshun94 <shunshun9460@gmail.com>
2024-03-31 21:04:05 +0900

    add APPLICATION_ID

--- python/myconfiguration.default.py
+++ python/myconfiguration.default.py
@@ -1,11 +1,13 @@
 ###########################
 # このファイルを myconfiguration.py にリネームし、
 # PUBLIC_KEY に Discord Application > General Information > PUBLIC KEY の値
-# ACCESS_TOKEN に Discord Application > Bot > TOKEN の値を代入して使用します
+# ACCESS_TOKEN に Discord Application > Bot > TOKEN の値
+# APPLICATION_ID に Discord Application > General Information > APPLICATION ID を代入して使用します
 # 
 
 PUBLIC_KEY = ""
 ACCESS_TOKEN = ""
+APPLICATION_ID = ""
 BROWSER_ACCESS_HEADER = "Discord app によるアクセスを受け付けるようにここは作られています"
 BROWSER_ACCESS_TEXT = "なのでブラウザでアクセスしても何も見せられません＞＜"
 MAX_LOG_GET_TIMES = 20
