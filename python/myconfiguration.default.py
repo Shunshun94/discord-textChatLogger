aec94ed7221e0d2dc954edd7f8d421b1cb73bf74
Shunshun94 <shunshun9460@gmail.com>
2024-04-05 03:39:27 +0900

    add param to link

--- python/myconfiguration.default.py
+++ python/myconfiguration.default.py
@@ -2,12 +2,14 @@
 # このファイルを myconfiguration.py にリネームし、
 # PUBLIC_KEY に Discord Application > General Information > PUBLIC KEY の値
 # ACCESS_TOKEN に Discord Application > Bot > TOKEN の値
-# APPLICATION_ID に Discord Application > General Information > APPLICATION ID を代入して使用します
+# APPLICATION_ID に Discord Application > General Information > APPLICATION ID
+# DOWNLOAD_PATH に このファイルが置かれているディレクトリのパス（https://xxxx/xxxx/xxxx） を代入して使用します
 # 
 
 PUBLIC_KEY = ""
 ACCESS_TOKEN = ""
 APPLICATION_ID = ""
+DOWNLOAD_PATH = ""
 BROWSER_ACCESS_HEADER = "Discord app によるアクセスを受け付けるようにここは作られています"
 BROWSER_ACCESS_TEXT = "なのでブラウザでアクセスしても何も見せられません＞＜"
 MAX_LOG_GET_TIMES = 20
